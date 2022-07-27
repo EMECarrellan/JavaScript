abc46a482dbb71ed18d954fea82eb38103a17a5e
EMECarrellan <105559539+EMECarrellan@users.noreply.github.com>
2022-07-27 18:56:29 +0200

    03-ejercicio-estructuras-de-control

---
+++ ejercicio-3-estructuras-de-control/factorial-break.js
@@ -0,0 +1,16 @@
+let i = 0;
+let factorial = 1;
+
+while (true) {
+        while (true) {
+            i++;
+            if (i <= 10) {
+                break
+            }
+        }
+        factorial *= i
+        if (i === 10) {
+            break
+        }
+}
+console.log(factorial)
---
+++ ejercicio-3-estructuras-de-control/factorial-while.js
@@ -0,0 +1,7 @@
+factorial = 1;
+i = 0;
+while (i < 10) {
+    i++;
+    factorial *= i;
+}
+console.log(factorial)
