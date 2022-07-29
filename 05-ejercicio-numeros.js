7073fad58866d7eec0dbd8633aa6920c40ed5963
EMECarrellan <105559539+EMECarrellan@users.noreply.github.com>
2022-07-29 21:14:28 +0200

    Add files via upload

---
+++ 05-ejercicio-numeros.js
@@ -0,0 +1,9 @@
+let alturaCm = 169;
+let alturaM = 1.69;
+let pesoKg = 50.7;
+
+alturaMR = alturaM.toFixed(0);
+pesoKgT = Math.floor(pesoKg);
+
+MaxValor = Number.MAX_VALUE;
+isMaxValor = Number.MaxValor + 1;
