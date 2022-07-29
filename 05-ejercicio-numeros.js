d18240554869476ddce69606f34f2b12531d2db0
EMECarrellan <105559539+EMECarrellan@users.noreply.github.com>
2022-07-29 21:19:30 +0200

    Update 05-ejercicio-numeros.js

--- 05-ejercicio-numeros.js
+++ 05-ejercicio-numeros.js
@@ -1,3 +1,23 @@
+/*
+
+Enunciado del ejercicio:
+
+Crea un archivo JS que contenga las siguientes líneas
+
+- Una variable que contenga tu altura en centímetros (entero)
+
+- Una variable que contenga tu altura en metros (número de coma flotante)
+
+- Una variable que contenga tu peso en kilogramos (número de coma flotante)
+
+- Una variable que contenga tu altura en metros redondeada hacia arriba
+
+- Una variable que contenga tu peso en kilogramos redondeado hacia abajo
+
+- Una variable que contenga si "el máximo valor que se puede obtener en Javascript + 1" es igual al máximo valor que se puede obtener en Javascript
+
+*/
+
 let alturaCm = 169;
 let alturaM = 1.69;
 let pesoKg = 50.7;
@@ -5,5 +25,4 @@ let pesoKg = 50.7;
 alturaMR = alturaM.toFixed(0);
 pesoKgT = Math.floor(pesoKg);
 
-MaxValor = Number.MAX_VALUE;
-isMaxValor = Number.MaxValor + 1;
+isMaxValor = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
